8784b3cd9a983447285bb82e73ef1a35e833192c
Mussa Athumani Matola <mussamatola@gmail.con>
2021-04-15 11:41:45 +0300

    Add: Sound key media control

--- config.def.h
+++ config.def.h
@@ -1,6 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
+static const char *upvol[] = { "amixer", "-q", "sset", "Master", "1%+", NULL };
+static const char *downvol[] = { "amixer", "-q", "sset", "Master", "1%-", NULL };
+static const char *mute[] = { "amixer", "-q", "-D", "pulse", "sset", "Master", \
+  "toggle", NULL };
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
@@ -73,6 +79,9 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+  { 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = upvol}},
+  { 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = downvol}},
+  { 0,                XF86XK_AudioMute,      spawn,          {.v = mute}},
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
